0d87cb7a645dbc8f71238ec6aab84f4a79c599b6
Gwalker1313 <111939848+Gwalker1313@users.noreply.github.com>
2024-01-15 22:12:47 -0700

    Walking sfx
    Made the timer work and inserted audio into the physics method for proper response times.

--- Godot stuff/Characters/test_player.gd
+++ Godot stuff/Characters/test_player.gd
@@ -9,6 +9,7 @@ func _ready():
 	update_animation_parameters(starting_direction)
 
 func _physics_process(delta):
+	#print($PlayerAudio/Timer.time_left)s
 	# Get Input direction
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
@@ -24,18 +25,26 @@ func _physics_process(delta):
 	else:
 		velocity = input_direction * move_speed
 	
+	# Use walking sfx if the player is moving.
+	if(velocity.length() == 0):
+		# If the player is not moving, play "idle" sfx.
+		#$PlayerAudio/Footstep_Gravel.play("Idle")
+		pass
+	elif($PlayerAudio/Timer.time_left <= 0.2):
+		$PlayerAudio/Footstep_Gravel.play()
+		$PlayerAudio/Timer.start(0.6)
 	
 	# Move and slide function uses velocity of character body to move character on map
 	move_and_slide()
 	pick_new_state()
 
 func update_animation_parameters(move_input : Vector2):
-	if(move_input != Vector2.ZERO):
+	if(velocity != Vector2.ZERO):
 		animation_tree.set("parameters/Walk/blend_position", move_input)
 		animation_tree.set("parameters/Idle/blend_position", move_input)
-	if(velocity != Vector2.ZERO):
-		$PlayerAudio/Footstep_Gravel.play()
-		$PlayerAudio/Timer.start(100)
+		#if($PlayerAudio/Timer.time_left <= 0):
+			#$PlayerAudio/Footstep_Gravel.play()
+			#$PlayerAudio/Timer.start(0.2)
 func pick_new_state():
 	if(velocity != Vector2.ZERO):
 		state_machine.travel("Walk")
